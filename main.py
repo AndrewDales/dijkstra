bb82bc3f5bda556e97c7048d267232dee17f439f
Andrew Dales <andrew.dales@highgateschool.org.uk>
2021-03-18 18:11:56 +0000

    Change 'connections' to 'edges'

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from math import inf
 
 
 # The dijkstra algorithm
-def dijkstra(nodes, connections, start):
+def dijkstra(nodes, edges, start):
     # Set up the priority queue
     priority_queue = {node: inf for node in nodes}
     priority_queue[start] = 0
@@ -20,10 +20,10 @@ def dijkstra(nodes, connections, start):
         visited[next_node] = next_dist
 
         # Find the neighbours of the newly added node
-        neighbours = {nd for nd in priority_queue if {nd, next_node} in set(connections.keys())}
+        neighbours = {nd for nd in priority_queue if {nd, next_node} in set(edges.keys())}
         #  For each unvisited neighbour of the new node check and if necessary update the distance to that neighbour
         for neighbour in neighbours:
-            min_dist = min(priority_queue[neighbour], next_dist + connections[frozenset({next_node, neighbour})])
+            min_dist = min(priority_queue[neighbour], next_dist + edges[frozenset({next_node, neighbour})])
             priority_queue[neighbour] = min_dist
 
         print(f'visited: {visited}')
@@ -34,14 +34,14 @@ def dijkstra(nodes, connections, start):
 
 if __name__ == "__main__":
     nodes_in = {'A', 'B', 'C', 'D', 'E'}
-    connections_in = {frozenset({'A', 'B'}): 7,
-                      frozenset({'A', 'D'}): 3,
-                      frozenset({'B', 'D'}): 2,
-                      frozenset({'B', 'C'}): 3,
-                      frozenset({'B', 'E'}): 6,
-                      frozenset({'C', 'D'}): 4,
-                      frozenset({'D', 'E'}): 7,
-                      frozenset({'C', 'E'}): 1}
+    edges_in = {frozenset({'A', 'B'}): 7,
+                frozenset({'A', 'D'}): 3,
+                frozenset({'B', 'D'}): 2,
+                frozenset({'B', 'C'}): 3,
+                frozenset({'B', 'E'}): 6,
+                frozenset({'C', 'D'}): 4,
+                frozenset({'D', 'E'}): 7,
+                frozenset({'C', 'E'}): 1}
 
-    min_paths = dijkstra(nodes_in, connections_in, 'A')
+    min_paths = dijkstra(nodes_in, edges_in, 'A')
     print(min_paths)
